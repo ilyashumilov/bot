e828e69823c72b6161c04022254973e0a73094b1
Ilya Shumilov <61500082+ilyashumilov@users.noreply.github.com>
2022-10-26 17:46:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ from messages import *
 
 token  =  os.getenv('TOKEN')
 bot=telebot.TeleBot (token)
-@bot.message_handler(commands=['start'])
-def start(message):
-      bot.send_message(message.chat.id, msg1, reply_markup=markup1)
+@bot.message_handler(content_types=['text'])
+def handler(message):
+      if message.text == '/start':
+            bot.send_message(message.chat.id, msg1, reply_markup=markup1)
+
+      if message.text == '🔎 Поиск':
+            bot.send_message(message.chat.id, msg2, reply_markup=markup2)
+
+
+
 bot.polling(none_stop=True)
