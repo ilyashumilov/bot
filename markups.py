eda49c80a3cdf0487ec6e5763f7bfcabbd4f4dad
Ilya Shumilov <61500082+ilyashumilov@users.noreply.github.com>
2022-10-26 17:47:15 +0300

    Update markups.py

--- markups.py
+++ markups.py
@@ -3,3 +3,12 @@ from telebot import types
 markup1 = types.ReplyKeyboardMarkup()
 item1 = types.KeyboardButton("🔎 Поиск")
 markup1.add(item1)
+
+
+markup2 = types.ReplyKeyboardMarkup()
+item1 = types.KeyboardButton("🌐 ВКонтакте")
+item2 = types.KeyboardButton("📷 Instagram")
+item3 = types.KeyboardButton("👤 Телеграм")
+item4 = types.KeyboardButton("📞 Номер телефона")
+markup2.add(item1,item2,item3,item4)
+
