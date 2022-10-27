36c2b4541d95a15b4dac6680629bc403874b0638
Ilya Shumilov <61500082+ilyashumilov@users.noreply.github.com>
2022-10-27 15:46:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,64 @@
-import os
-import time
+import requests as r
 import telebot
-from markups import *
-from messages import *
+import time
 
-token  =  os.getenv('TOKEN')
-token1  =  os.getenv('TOKEN1')
+coins = ['IOTA','LUNC','SUSHI']
 
+token = '5766858406:AAGswKF8IfZapYhr3894fj-mi9ospJteprU'
+key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjM1YTZlNTVmYzVhOGFkZmVjNjI0OTExIiwiaWF0IjoxNjY2ODcwODY5LCJleHAiOjMzMTcxMzM0ODY5fQ.lrl5ZJrL046hA5dOmwjy3FrLInXoX3ce6FP6N33Sup4'
 bot=telebot.TeleBot(token)
-bot1=telebot.TeleBot(token1)
 
-@bot.channel_post_handler(func=lambda m: True,content_types=['text'])
+@bot.message_handler(content_types=['text'])
 def handler(message):
       if message.text == '/start':
-            bot.send_message(message.chat.id, msg1, reply_markup=markup1)
-
-      elif message.text == '🔎 Поиск':
-            bot.send_message(message.chat.id, msg2, reply_markup=markup2)
-
-      elif message.text == '10':
-            # bot.send_message(message.chat.id, msg8)
-            # time.sleep(2)
-            bot.send_message(message.chat.id, msg9)
-            bot1.send_message(message.chat.id, 'bot1')
-
-      else:
-            bot.send_message(message.chat.id, msg7)
-
-@bot.callback_query_handler(lambda query: query.data in ["vk"])
-def process_callback_2(query):
-      bot.send_message(query.message.chat.id, msg3)
-
-@bot.callback_query_handler(lambda query: query.data in ["ig"])
-def process_callback_2(query):
-      bot.send_message(query.message.chat.id, msg4)
-
-@bot.callback_query_handler(lambda query: query.data in ["tg"])
-def process_callback_2(query):
-      bot.send_message(query.message.chat.id, msg5)
-
-@bot.callback_query_handler(lambda query: query.data in ["no"])
-def process_callback_2(query):
-      bot.send_message(query.message.chat.id, msg6)
+            bot.send_message(message.chat.id,'message.chat.id')
 
 bot.polling(none_stop=True)
+
+
+# import os
+# import time
+# import telebot
+# from markups import *
+# from messages import *
+
+# token  =  os.getenv('TOKEN')
+# token1  =  os.getenv('TOKEN1')
+
+# bot=telebot.TeleBot(token)
+# bot1=telebot.TeleBot(token1)
+
+# @bot.channel_post_handler(func=lambda m: True,content_types=['text'])
+# def handler(message):
+#       if message.text == '/start':
+#             bot.send_message(message.chat.id, msg1, reply_markup=markup1)
+
+#       elif message.text == '🔎 Поиск':
+#             bot.send_message(message.chat.id, msg2, reply_markup=markup2)
+
+#       elif message.text == '10':
+#             # bot.send_message(message.chat.id, msg8)
+#             # time.sleep(2)
+#             bot.send_message(message.chat.id, msg9)
+#             bot1.send_message(message.chat.id, 'bot1')
+
+#       else:
+#             bot.send_message(message.chat.id, msg7)
+
+# @bot.callback_query_handler(lambda query: query.data in ["vk"])
+# def process_callback_2(query):
+#       bot.send_message(query.message.chat.id, msg3)
+
+# @bot.callback_query_handler(lambda query: query.data in ["ig"])
+# def process_callback_2(query):
+#       bot.send_message(query.message.chat.id, msg4)
+
+# @bot.callback_query_handler(lambda query: query.data in ["tg"])
+# def process_callback_2(query):
+#       bot.send_message(query.message.chat.id, msg5)
+
+# @bot.callback_query_handler(lambda query: query.data in ["no"])
+# def process_callback_2(query):
+#       bot.send_message(query.message.chat.id, msg6)
+
+# bot.polling(none_stop=True)
